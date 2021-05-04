2d8a38f8eff0ea51ebf14b499383cd2c23a05737
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-05-03 17:48:05 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, VERSION as ngVersion } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'aacli-v11211';
+  public get angularVersion(): string {
+    return ngVersion.full;
+  }
 }
